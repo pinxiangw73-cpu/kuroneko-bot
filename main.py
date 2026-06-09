693390e157ee7474f385e0266ed34c872eddfc39
pinxiangw73-cpu <pinxiangw73@gmail.com>
2026-06-09 21:11:21 +0800

    Refactor bot token handling and update start message

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = "8695440286:AAHW04Ejm-bQya3xLjVLIx-gFuuFfGidaAY"
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# /start 指令
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("我醒了")
+    await update.message.reply_text("喵～機器人已啟動！")
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
 
-    print("bot running")
+    # ⭐ 最重要這一行（一定要有）
+    app.run_polling(
+        drop_pending_updates=True
+    )
 
-    app.run_polling(drop_pending_updates=True)
-    
 if __name__ == "__main__":
     main()
